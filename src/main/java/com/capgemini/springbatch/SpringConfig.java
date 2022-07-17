ef8fee1c62612a441ab69a0d4ebba13884f30d28
Hleb Shypula <glebszypula1997@gmail.com>
2022-07-17 20:28:45 +0200

    Update SpringConfig.java

--- src/main/java/com/capgemini/springbatch/SpringConfig.java
+++ src/main/java/com/capgemini/springbatch/SpringConfig.java
@@ -9,58 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableBatchProcessing
 public class SpringConfig {
 
-//    @Value("org/springframework/batch/core/schema-drop-sqlite.sql")
-//    private Resource dropRepositoryTables;
-//
-//    @Value("org/springframework/batch/core/schema-sqlite.sql")
-//    private Resource dataRepositorySchema;
-//
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.sqlite.JDBC");
-//        dataSource.setUrl("jdbc:sqlite:repository.sqlite");
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public DataSourceInitializer dataSourceInitializer(DataSource dataSource)
-//            throws MalformedURLException {
-//        ResourceDatabasePopulator databasePopulator =
-//                new ResourceDatabasePopulator();
-//
-//        databasePopulator.addScript(dropRepositoryTables);
-//        databasePopulator.addScript(dataRepositorySchema);
-//        databasePopulator.setIgnoreFailedDrops(true);
-//
-//        DataSourceInitializer initializer = new DataSourceInitializer();
-//        initializer.setDataSource(dataSource);
-//        initializer.setDatabasePopulator(databasePopulator);
-//
-//        return initializer;
-//    }
-//
-//    private JobRepository getJobRepository() throws Exception {
-//        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
-//        factory.setDataSource(dataSource());
-//        factory.setTransactionManager(getTransactionManager());
-//        factory.afterPropertiesSet();
-//        return (JobRepository) factory.getObject();
-//    }
-//
-//    private PlatformTransactionManager getTransactionManager() {
-//        return new ResourcelessTransactionManager();
-//    }
-//
-//    public JobLauncher getJobLauncher() throws Exception {
-//        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-//        jobLauncher.setJobRepository(getJobRepository());
-//        jobLauncher.afterPropertiesSet();
-//        return jobLauncher;
-//    }
 }
